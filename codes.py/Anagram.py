ae6e151bfe5b70fae1bc7c30eeb6eca7449b5d33
Himani Dhawan <65930020+dhawan-mani@users.noreply.github.com>
2024-05-16 12:12:35 -0700

    Update Anagram.py
    Updated the code to run faster!

--- codes.py/Anagram.py
+++ codes.py/Anagram.py
@@ -1,17 +1,11 @@
 #Anagrams are strings that have the same no. of characters in the string and teh same length but the order of strings are different.
 a = input("enter the string")
 b = input("enter the string")
-index = 0
-result = 1
-flag = 1
-n1 = len(a)
-n2 = len(b)
-if(n1==n2):
-    for x in a:
-        if x not in b:
-            flag=0
-            break
-    if flag == 1:
-        print("string is anagram")
+def is_anagram(str1,str2):
+    str1= str1.replace(" ","").lower()
+    str2 = str2.replace(" ","").lower()
+    if sorted(str1) == sorted(str2):
+        print("Hurray!! It's anagram!!")
     else:
-        print("not ")
+        print("Dang!! It's Not!")
+is_anagram(a,b)
