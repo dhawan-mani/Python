bb3df9f819bbd2aa4cc788f9cfd2be49010c9ff1
Himani Dhawan <65930020+dhawan-mani@users.noreply.github.com>
2024-05-16 15:08:45 -0700

    Update palindrome.py
    The updated code check if the characters are alphanumeic,converts them in to lowercase and then join results of a list into string.
    That ways more no. of edge cases are added.
    e.g.:print(is_palindrome("radar"))  # True
    print(is_palindrome("level"))  # True
    print(is_palindrome("12321"))  # True
    print(is_palindrome("A man, a plan, a canal, Panama!"))  # True
    print(is_palindrome("hello"))  # False

--- codes.py/palindrome.py
+++ codes.py/palindrome.py
@@ -1,7 +1,8 @@
-import him
-a= input("enter a no.")
-if(a==a[::-1]):
-    print("palindrome")
-else:
-    print("Not a palindrome")
-#him.main()
+string = input("Enter the string fore palindrome")
+def is_palindrome(s):
+    s =''.join(x.lower() for x in s if x.isalnum())
+    if s == s[::-1]:
+        print("It's a palindrome")
+    else:
+        print("It's not a palindrome")
+is_palindrome(string)
